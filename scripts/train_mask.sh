036bab088d646eb4f2c08fb877837b986a44f5a0
Mukilan Krishnakumar <mukilan.git@gmail.com>
2023-09-26 12:51:40 +0530

    Scripts for training Mask-RCNN and Fast-RCNN

---
+++ scripts/train_mask.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ../tools
+
+python train_net.py \
+    --dataset_name          annots \
+    --json_annotation_train annots/train.json \
+    --image_path_train      annots/images \
+    --json_annotation_val   annots/test.json \
+    --image_path_val        annots/images \
+    --config-file           ../configs/prima/mask_rcnn_R_50_FPN_3x.yaml \
+    OUTPUT_DIR  ../outputs/prima/mask_rcnn_R_50_FPN_3x/ \
+    SOLVER.IMS_PER_BATCH 2 
